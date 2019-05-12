51624b38874c4680300e007d8f646d977232d9d8
Keerthana.Bathrinath@cognizant.com <keerthana.b@syfore.com>
2019-05-12 23:44:33 +0530

    update readme and config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,8 @@ module.exports = {
   users: {
     admin: {password: 'password', scopes: ''},
     stranger: {password: 'password', scopes: ''},
-    shankar: {password: 'password', scopes: ''}
+    shankar: {password: 'password', scopes: ''},
+    kumar: {password: 'password', scopes: ''}
   },
   jwtSecret: '08098grrgaorugh',
   port: 3000,
